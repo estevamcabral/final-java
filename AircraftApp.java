780789b9e9d623ea69d9ad8d6c88ac3007da46ca
Fernanda Franceschini <fefe.franceschini@gmail.com>
2023-06-19 10:37:11 -0300

    teste

--- AircraftApp.java
+++ AircraftApp.java
@@ -21,16 +21,27 @@ public class AircraftApp
         System.out.printf("[2]Porto Alegre--> São Paulo\n");
         System.out.printf("[3]Porto Alegre--> Recife\n");
         int destination= sc.nextInt();
-         String nameDestination;
-         String hour;
-         String numVoo;
+         String nameDestination="";
+         String hour="";
+         String numVoo="";
         if(destination==1){
-            System.out.println("Venda de passagens");
             nameDestination= "Rio de Janeiro";
             hour= "06:30";
             numVoo= "TAM3434";
-            String command;
-            do {
+        }
+        if(destination==2){
+            nameDestination= "São Paulo";
+            hour= "08:45";
+            numVoo= "TAM2173";
+        }
+        if(destination==3){
+            nameDestination= "Recife";
+            hour= "19:30";
+            numVoo= "TAM6060";
+        }
+        System.out.println("Venda de passagens");
+        String command;
+        do {
             command= sc.nextLine();
             if (command.startsWith("show"))
                 destiny[0].print(nameDestination, hour,numVoo);
@@ -43,46 +54,5 @@ public class AircraftApp
             else
                 System.out.println("Comando invalido!");
             } while (!command.startsWith("quit"));
-        }
-        if(destination==2){
-            System.out.println("Venda de passagens");
-            String command;
-            nameDestination= "São Paulo";
-            hour= "08:45";
-            numVoo= "TAM2173";
-            do {
-                command = sc.nextLine();
-                if (command.startsWith("show"))
-                destiny[1].print(nameDestination, hour,numVoo);
-                else if (command.startsWith("sell"))                
-                destiny[1].sell(command);
-                else if (command.startsWith("write"))                
-                destiny[1] .write(numVoo);
-                else if (command.startsWith("read"))                
-                destiny[1].read();
-                else if (!command.startsWith("quit"))
-                System.out.println("Comando invalido!");
-            } while (!command.startsWith("quit"));
-        }
-        if(destination==3){
-            String command;
-            System.out.println("Venda de passagens");
-            nameDestination= "Recife";
-            hour= "19:30";
-            numVoo= "TAM6060";
-            do {
-                command = sc.nextLine();
-                if (command.startsWith("show"))
-                destiny[2].print(nameDestination, hour,numVoo);
-                else if (command.startsWith("sell"))                
-                destiny[2].sell(command);
-                else if (command.startsWith("write"))                
-                destiny[2] .write(numVoo);
-                else if (command.startsWith("read"))                
-                destiny[2].read();
-                else if (!command.startsWith("quit"))
-                System.out.println("Comando invalido!");
-            } while (!command.startsWith("quit"));
-        }
     }
 }
